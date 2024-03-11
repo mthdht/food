20be087867649fa08b91c37aca17df7e9667183a
mthdht <mthdht@gmail.com>
2024-03-11 10:20:13 +0100

    change app name in config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'food',
+  appName: 'YADURAB',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
