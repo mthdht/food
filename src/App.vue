e4548220666e2b19a516a3da4a6045b3e711c09a
mthdht <mthdht@gmail.com>
2024-03-11 13:45:16 +0100

    change icon on tabs bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>YADURAB</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-router-outlet />
   </ion-app>
 </template>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,18 +4,19 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" size="large"></ion-icon>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2" color="warning">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="map" size="large"></ion-icon>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon aria-hidden="true" :icon="heart" size="large"/>
+        </ion-tab-button>
+
+      <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon aria-hidden="true" :icon="personCircle" size="large"/>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +25,5 @@
 
 <script setup>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {  home, map, heart, personCircle } from 'ionicons/icons';
 </script>
